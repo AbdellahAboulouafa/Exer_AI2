05c0b5e698120c2f772714066d947f5bfbcf07ec
AbdellahAboulouafa <aboulouaafa.abdelllah@gmail.com>
2026-02-25 15:37:31 -0500

    test

--- Source/Tool/obj/Debug/net9.0/Tool.AssemblyInfo.cs
+++ Source/Tool/obj/Debug/net9.0/Tool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5bb6610b8bd70a7c2e7558960284c595a6e0a5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b4c69f286decd4568d85d1abd56fbb0eb9396d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
